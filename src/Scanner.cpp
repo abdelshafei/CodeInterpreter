5965f7252ea691b322fd1c10dcfafbc359a96e10
Abdelrahman Elshafei <abdelrahmanelshafei@Abdelrahman-ElShafay.local>
2024-12-16 01:30:00 -0500

    Second Stage complete

--- src/Scanner.cpp
+++ src/Scanner.cpp
@@ -21,7 +21,7 @@ void Scanner::addToken(TokenType type, any literal) {
     if(type == END_OF_FILE) {
         text = "";
     } else {
-        text = src.substr(start, current);
+        text = src.substr(current, current);
     }
     tokens.push_back(new Token(type, text, literal, line));
 }
