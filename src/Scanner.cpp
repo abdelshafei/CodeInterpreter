731a73f357b4b2b90af9867de7ed8da4f559a3ca
Abdelrahman Elshafei <abdelrahmanelshafei@Abdelrahman-ElShafay.local>
2024-12-17 18:51:05 -0500

    string literals complete

--- src/Scanner.cpp
+++ src/Scanner.cpp
@@ -11,7 +11,7 @@ Scanner::Scanner(const string& src) : src(src)
 
 string Scanner::getStringLiteral() {
     string stringBuilder = "";
-    for(int i = current+1; i < src.size(); i++) {
+    for(int i = current; i < src.size(); i++) {
         if(src.at(i) = '"') {
             current = i+1;
             break;
