fc198f38e4381730e3b12eab4ca18aa550442302
Abdelrahman Elshafei <abdelrahmanelshafei@AbdelraElShafay.ht.home>
2024-12-20 23:57:37 -0500

    Parsing Stage

--- src/Parser/AstPrinter.cpp
+++ src/Parser/AstPrinter.cpp
@@ -2,19 +2,19 @@
 
 #include <sstream>
 
-string AstPrinter::print(Expr expression) {
+string AstPrinter::print(Expr expression) const {
     return expression.accept(*this);
 }
 
-string AstPrinter::visitBinaryExpr(const Binary& expr) {
+string AstPrinter::visitBinaryExpr(const Binary& expr) const {
     return parenthesize(expr.oprator.lexeme, expr.left, expr.right);
 }
 
-string AstPrinter::visitGroupingExpr(const Grouping& expr) {
+string AstPrinter::visitGroupingExpr(const Grouping& expr) const {
     return parenthesize("group", expr.expression);
 }
 
-string AstPrinter::visitLiteralExpr(const Literal& expr) {
+string AstPrinter::visitLiteralExpr(const Literal& expr) const {
     return visit([](const auto& val) -> string {
         if constexpr (is_same_v<decay_t<decltype(val)>, string>) {
             return val; 
@@ -27,12 +27,12 @@ string AstPrinter::visitLiteralExpr(const Literal& expr) {
     }, expr.value);
 }
 
-string AstPrinter::visitUnaryExpr(const Unary& expr) {
+string AstPrinter::visitUnaryExpr(const Unary& expr) const {
     return parenthesize(expr.oprator.lexeme, expr.right);
 }
 
 template<typename... Expr>
-string AstPrinter::parenthesize(const string& name, Expr... exprs) {
+string AstPrinter::parenthesize(const string& name, Expr... exprs) const {
     string strBuilder;
 
     strBuilder += "(";
