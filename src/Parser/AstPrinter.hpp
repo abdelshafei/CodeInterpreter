d43a54081fe09d5eda52de39687cc0d5ddb72928
Abdelrahman Elshafei <abdelrahmanelshafei@AbdelraElShafay.ht.home>
2024-12-21 16:35:57 -0500

    Parsing Stage

--- src/Parser/AstPrinter.hpp
+++ src/Parser/AstPrinter.hpp
@@ -5,13 +5,13 @@
 
 class AstPrinter : public Expr::Visitor {
     public:
-        string print(Expr* expression);
+        string print(Expr* expression) const;
         string visitBinaryExpr(const Binary& expr) const override;
         string visitGroupingExpr(const Grouping& expr) const override;
         string visitLiteralExpr(const Literal& expr) const override;
         string visitUnaryExpr(const Unary& expr) const override;
         template<typename... Expr>
-        string parenthesize(const string& name, Expr*... exprs);
+        string parenthesize(const string& name, Expr*... exprs) const;
 };
 
 #endif
