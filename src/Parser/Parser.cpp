1828f6e97be152699f4df841c28b8906604a8766
Abdelrahman Elshafei <abdelrahmanelshafei@AbdelraElShafay.ht.home>
2024-12-23 03:26:57 -0500

    Parsing Stage

--- src/Parser/Parser.cpp
+++ src/Parser/Parser.cpp
@@ -129,7 +129,7 @@ Expr* Parser::primary() {
         expressions.push_back(groupedExpr);
         return groupedExpr;
     } else if(matchTypes(MINUS, PLUS, SLASH, STAR)) {
-        // consume(NUMBER, "Expect a number after expression");
+        cout << "here before out of range" << endl;
         if((previous()->type != NUMBER && peekAfter()->type != NUMBER) || (previous()->type != STRING && peekAfter()->type != STRING)) {
             throw err(peekAfter(), "Expect a number after expression");
         }
