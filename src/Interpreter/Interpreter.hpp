1f86c8562de3e3b2705298d9500b45c6166ac4d2
Abdelrahman Elshafei <abdelrahmanelshafei@AbdelraElShafay.ht.home>
2024-12-31 09:12:09 -0500

    running stage

---
+++ src/Interpreter/Interpreter.hpp
@@ -0,0 +1,34 @@
+#ifndef INTERPRETER_H
+#define INTERPRETER_H
+
+#include "../Parser/Expr.hpp"
+#include "../Env/Stmt.hpp"
+
+using Value = variant<bool, double, string>;
+
+class Interpreter : public Stmt::Visitor {
+    public:
+        void visitPrintStmt(const Print& stmt) override;
+        void visitExpressionStmt(const Expression& stmt) override;
+
+        T evaluate (const Expr& expr) ;
+        T evalBinaryExpr(const Binary& expr);
+        T evalGroupingExpr(const Grouping& expr);
+        T evalLiteralExpr(const Literal& expr);
+        T evalUnaryExpr(const Unary& expr);
+
+        bool isTruthy(T value) const;
+        string to_string(const T& value) const;
+        string NormalizeDouble(const string& txt) const;
+        T Product(T lValue, T rValue) const;
+        T Quotient(T lValue, T rValue) const;
+        T Difference(T lValue, T rValue) const;
+        T Sum(T lValue, T rValue) const;
+        bool isLogical(T lValue, T rValue, TokenType relational) const;
+
+        void interpret(vector<Stmt*> statements);
+        void excute(const Stmt& statement);
+
+};
+
+#endif
